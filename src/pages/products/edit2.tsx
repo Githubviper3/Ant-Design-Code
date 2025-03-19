70e94dbddde3443970ad9e9d307353dfdc88387f
Githubviper3 <Decland366@gmail.com>
2025-03-19 16:53:33 +0000

    Got Paging next is tables

--- src/pages/products/edit2.tsx
+++ src/pages/products/edit2.tsx
@@ -1,30 +1,85 @@
-import { useOne, useUpdate } from "@refinedev/core";
+import { useForm, useSelect } from "@refinedev/core";
 import { I_id } from "../../interfaces";
 
 
-export const EditProduct: React.FC<I_id> = ({ input_id }) => {
-  const { data, isLoading } = useOne({ resource: "products", id: input_id });
-  const { mutate, isLoading: isUpdating } = useUpdate();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
+export const EditProductByID: React.FC<I_id> = ({ input_id }) => {
+  if (input_id == null){
+    input_id = 123
   }
-
-  const updatePrice = async () => {
-    await mutate({
+  const { onFinish, mutation, query } = useForm({
+      action: "edit",
       resource: "products",
       id: input_id,
-      values: {
-        price: Math.floor(Math.random() * 100),
-      },
     });
-  };
+  
+    const record = query?.data?.data;
+  
+    const { options } = useSelect({
+      resource: "categories",
+    });
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      // Using FormData to get the form values and convert it to an object.
+      const data = Object.fromEntries(new FormData(event.currentTarget).entries());
+      // Calling onFinish to submit with the data we've collected from the form.
 
-  return (
-    <div>
-      <div>Product name: {data?.data.name}</div>
-      <div>Product price: ${data?.data.price}</div>
-      <button onClick={updatePrice}>Update Price</button>
-    </div>
-  );
+      // onFinish({
+      //   ...data,
+      //   price: Number(data.price).toFixed(2),
+      //   category: { id: Number(data.category) },
+      // });
+    };
+  
+    return (
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input type="text" id="name" name="name" defaultValue={record?.name} />
+        <br />
+        <label htmlFor="description">Description</label>
+        <br />
+        <textarea
+          id="description"
+          name="description"
+          defaultValue={record?.description}
+        />
+        <br />
+        <label htmlFor="price">Price</label>
+        <br />
+        <input
+          type="text"
+          id="price"
+          name="price"
+          pattern="\d*\.?\d*"
+          defaultValue={record?.price}
+        />
+        <br />
+        <label htmlFor="material">Material</label>
+        <br />
+        <input
+          type="text"
+          id="material"
+          name="material"
+          defaultValue={record?.material}
+        />
+        <br />
+        <label htmlFor="category">Category</label>
+        <br />
+        <select id="category" name="category">
+          {options?.map((option) => (
+            <option
+              key={option.value}
+              value={option.value}
+              selected={record?.category.id == option.value}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <br />
+  
+        {mutation.isSuccess && <span>successfully submitted!</span>}
+        <button type="submit">Submit</button>
+      </form>
+    );
 };
